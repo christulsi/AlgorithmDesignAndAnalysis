56cc7bb7742dcb642df2e1adb222396436f1d568
chris tulsi <christulsi@live.com>
2018-11-17 16:47:32 +0800

    add code to accept adjacency matrix input

--- DepthFirstSearchTraversalGraph.c
+++ DepthFirstSearchTraversalGraph.c
@@ -3,25 +3,28 @@
 void DFS(int);
 				
 int visited[4],n,v;    //n is no of vertices ,v is starting vertice and the graph is array matrix[4][4]
-int matrix[4][4] = {
-					{0,1,0,1},	//1
-					{1,0,1,1},	//2
-					{0,1,0,1},	//3
-					{1,1,1,0}	//4
-					};
+int matrix[4][4]; 
+// {
+// 					{0,1,0,1},	//1
+// 					{1,0,1,1},	//2
+// 					{0,1,0,1},	//3
+// 					{1,1,1,0}	//4
+// 					};
  
 void main()
 {
-    int i,j;					
+    int i,j;	
+    //printf("\nEnter number of vertices:");				
     scanf("%d",&n);             //number of vertices
+    //printf("\nEnter starting vertice:");
     scanf("%d",&v);             //starting vertice
 			
-    // //read the adjecency matrix
-    // printf("\nEnter adjecency matrix of the graph:");
+    //read the adjecency matrix
+    //printf("\nEnter adjecency matrix of the graph:");
    
-    // for(i=0;i<n;i++)
-    //    for(j=0;j<n;j++)
-    //         scanf("%d",&matrix[i][j]);
+    for(i=0;i<n;i++)
+       for(j=0;j<n;j++)
+            scanf("%d",&matrix[i][j]);
  
 	//visited is initialized to zero
     for(i=0;i<n;i++)
