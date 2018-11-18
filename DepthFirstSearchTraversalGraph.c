52e6a1dfedffff816e5da58de7baa58be5c837e0
chris tulsi <christulsi@live.com>
2018-11-18 21:25:00 +0800

    convert dfs from c to c++

--- DepthFirstSearchTraversalGraph.c
+++ DepthFirstSearchTraversalGraph.c
@@ -2,44 +2,40 @@
  
 void DFS(int);
 				
-int visited[4],n,v;    //n is no of vertices ,v is starting vertice and the graph is array matrix[4][4]
-int matrix[4][4]; 
-// {
-// 					{0,1,0,1},	//1
-// 					{1,0,1,1},	//2
-// 					{0,1,0,1},	//3
-// 					{1,1,1,0}	//4
-// 					};
- 
-void main()
+int visited[256],n,v;  
+int matrix[256][256];
+
+int main()
 {
     int i,j;	
-    //printf("\nEnter number of vertices:");				
-    scanf("%d",&n);             //number of vertices
-    //printf("\nEnter starting vertice:");
-    scanf("%d",&v);             //starting vertice
+   		
+    scanf("%d",&n);           
+    scanf("%d",&v);   
 			
-    //read the adjecency matrix
-    //printf("\nEnter adjecency matrix of the graph:");
-   
-    for(i=0;i<n;i++)
-       for(j=0;j<n;j++)
-            scanf("%d",&matrix[i][j]);
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+           scanf("%d",&matrix[i][j]);
+       }
+    }       
  
-	//visited is initialized to zero
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++){
         visited[i]=0;
- 
+    }
+        
     DFS(v-1);
+
+    return 0;
 }
  
 void DFS(int i)
 {
     int j;
     printf("%d ",i+1);
-    visited[i]=1; 							//set v to visited 
-    
-    for(j=0;j<n;j++)
-       if(!visited[j]&&matrix[i][j]==1)		// if v is not visited then explore
+    visited[i]=1; 		
+    				
+    for(j=0;j<n;j++){
+        if(!visited[j]&&matrix[i][j]==1)	
             DFS(j);
+    }
+      
 }
--- DepthFirstSearchTraversalGraph.cpp
+++ DepthFirstSearchTraversalGraph.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 
 using namespace std;
+ 
+void DFS(int);
 
-int main(int argc, char const *argv[])
+int n, v;	
+int visited[100];  
+int matrix[100][100];
+
+int main(void)
 {
+   
 
-	return 0;
+    cin >> n >> v;   
+			
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           cin >> matrix[i][j];
+       }
+    }       
+ 
+    for(int i=0;i<n;i++){
+        visited[i]=0;
+    }
+        
+    DFS(v-1);
+
+    return 0;
+}
+ 
+void DFS(int i)
+{
+    cout << (i+1) << " ";
+    
+    visited[i]=1; 		
+    				
+    for(int j=0;j<n;j++){
+        if(!visited[j]&&matrix[i][j]==1)	
+            DFS(j);
+    }
+      
 }
