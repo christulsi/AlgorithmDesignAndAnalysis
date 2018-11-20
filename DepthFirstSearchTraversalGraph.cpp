73b84fd0cfe480545d32acb12d879bce3772cfb2
chris tulsi <christulsi@live.com>
2018-11-20 11:25:38 +0800

    add sum to dfs

--- DepthFirstSearchTraversalGraph.cpp
+++ DepthFirstSearchTraversalGraph.cpp
@@ -1,43 +1,49 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
- 
+
 void DFS(int);
+int isVisited[100]={0}; //initialize with zeros
+int Edges[100][100];
+int sum = 0;
+int n,v;
 
-int n, v;	
-int visited[100];  
-int matrix[100][100];
-
-int main(void)
-{
-   
-
-    cin >> n >> v;   
-			
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-           cin >> matrix[i][j];
-       }
-    }       
+int main(){
  
-    for(int i=0;i<n;i++){
-        visited[i]=0;
-    }
-        
-    DFS(v-1);
+    cin>>n>>v;//n:no of vertices and v is starting vertex
 
+    //user input for adjacency matrix
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>Edges[i][j];
+        }
+        isVisited[i]=0;
+    }
+
+    DFS(v); 
+
+    for(int i=1;i<=n&&sum<n;i++){
+            if(isVisited[i]==0){
+                isVisited[i]=1;
+                DFS(i);
+            }
+    }
     return 0;
 }
- 
-void DFS(int i)
-{
-    cout << (i+1) << " ";
+
+//Depth First Search Travesal
+void DFS(int node){
     
-    visited[i]=1; 		
-    				
-    for(int j=0;j<n;j++){
-        if(!visited[j]&&matrix[i][j]==1)	
-            DFS(j);
-    }
-      
+    cout<<node<<" ";
+    sum++;
+
+    isVisited[node]=1;//assigned as visited node
+        for(int j=1;j<=n;j++)
+            if(isVisited[j]==0 && Edges[node][j]==1){
+                isVisited[j]=1;
+                DFS(j);//recursion
+            }
+        
 }
+
+
